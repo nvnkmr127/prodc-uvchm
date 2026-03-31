c5702b1362ed23fce70399b63c79a56c178b4510
Naveen <nvnkmr127@gmail.com>
2026-03-31 09:52:10 +0530

    dwwqqwd

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\NotificationServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
     App\Providers\ViewServiceProvider::class,
 ];
