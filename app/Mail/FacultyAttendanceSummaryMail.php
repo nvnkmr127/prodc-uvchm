3bdb5eec037385eb27c3e874d16c387b488db0b5
Naveen <nvnkmr127@gmail.com>
2026-06-22 16:59:04 +0530

    refactor: remove ShouldQueue interface from faculty attendance summary mail classes to disable background queuing

--- app/Mail/FacultyAttendanceSummaryMail.php
+++ app/Mail/FacultyAttendanceSummaryMail.php
@@ -3,11 +3,10 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class FacultyAttendanceSummaryMail extends Mailable implements ShouldQueue
+class FacultyAttendanceSummaryMail extends Mailable
 {
     use Queueable, SerializesModels;
 
